ba06863ee2e0562ef6acc4a298c3ffff5944325d
unto nordling <unto.nordling@metropolia.fi>
2022-08-22 14:09:39 +0300

    viisi suurinta lukua

---
+++ 5.2.py
@@ -0,0 +1,13 @@
+luku = 0
+luvut = []
+i = 0
+while luku != "":
+    luku = input("Anna luku: ")
+    if luku == "":
+        break
+
+    luku = int(luku)
+    luvut.append(luku)
+for i in range(5):
+    luvut.sort(reverse=True)
+    print(luvut[i])
