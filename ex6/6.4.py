0ceb7391c8f4da0640acf3d43d28058bcd223476
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 23:56:06 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex6/6.4.py
+++ ex6/6.4.py
@@ -1,9 +1,13 @@
-def listan_summa(luvut: list):
-    summa = 0
-    for i in range(len(luvut)):
-        summa += luvut[i]
-    return summa
+# a simple function that gives the sum of the list (without using the sum()-function)
 
-lista = [11, 4, 5, 15, 7]
-tulos = listan_summa(lista)
-print(tulos)
+def list_sum(list_of_numbers: list):
+    sum_of_numbers = 0
+    for i in range(len(list_of_numbers)):
+        sum_of_numbers += list_of_numbers[i]
+    return sum_of_numbers
+
+
+# the main program tests the function
+list_of_numbers_main = [11, 4, 5, 15, 7]
+sum_of_list = list_sum(list_of_numbers_main)
+print(sum_of_list)
