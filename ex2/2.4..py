3f46d3ccec75366b245065ed750862df33a65302
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 19:11:04 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex2/2.4..py
+++ ex2/2.4..py
@@ -1,5 +1,9 @@
-eka = int(input("Anna luku:" ))
-toka = int(input("Anna luku:" ))
-kolmas = int(input("Anna luku:" ))
+# a simple program that calculates sum, product and mean of the numbers
 
-print(f"lukujen summa on: {eka + toka + kolmas}\n tulo on: {eka * toka * kolmas}\n keskiarvo on: {(eka + toka + kolmas) / 3}")
+num1 = int(input("Give a number:" ))
+num2 = int(input("Give a number:" ))
+num3 = int(input("Give a number:" ))
+
+print(f"sum: {num1 + num2 + num3}\n"
+      f"product: {num1 * num2 * num3}\n"
+      f"mean: {(num1 + num2 + num3) / 3}")
