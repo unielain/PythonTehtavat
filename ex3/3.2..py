63225a90b68e5d4dec8720bb5128443b6cdf25fe
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 19:49:10 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex3/3.2..py
+++ ex3/3.2..py
@@ -1,13 +1,23 @@
-hyttiluokka = input("Kerro hyttiluokka: ")
-if hyttiluokka == "LUX":
-    print("LUX on parvekkeellinen hytti yläkannella.")
-elif hyttiluokka == "A":
-    print("A on ikkunallinen hytti autokannen yläpuolella.")
-elif hyttiluokka == "B":
-    print("B on ikkunaton hytti autokannen yläpuolella.")
-elif hyttiluokka == "C":
-    print("C on ikkunaton hytti autokannen alapuolella.")
-else:
-    print("Virheellinen hyttiluokka")
+# a program that tells the user which class of cabins they occupy
+# LUX: is a cabin with a balcony in the upper deck.
+# A: is a cabin with a window above the parking deck.
+# B: is a cabin without a window above the parking deck.
+# C: is a cabin without a window below the parking deck.
 
+classes_explained = {"LUX": "LUX: is a cabin with a balcony in the upper deck.",
+           "A": "A: is a cabin with a window above the parking deck.",
+           "B": "B: is a cabin without a window above the parking deck.",
+           "C": "C: is a cabin without a window below the parking deck."}
 
+# at first we must check if the user's input is correct
+while True:
+    cabin_class = input("Please, insert your cabin class (LUX, A, B, C): ")
+    cabin_class = cabin_class.upper()
+    if cabin_class in classes_explained:
+        break
+    else:
+        print(f"Cabin class {cabin_class} could not be found.\n")
+
+for cabin in classes_explained:
+    if cabin == cabin_class:
+        print(classes_explained[cabin])
