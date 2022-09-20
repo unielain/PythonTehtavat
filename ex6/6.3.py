afb14e507c4bdeb4d559ec204ef6361c66336486
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 23:50:22 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex6/6.3.py
+++ ex6/6.3.py
@@ -1,10 +1,14 @@
-def gallona_litroiksi(gal: int):
-    litra = gal * 3.78541178
-    return litra
-gallona = 0
-while gallona >= 0:
-    gallona = int(input("Anna määrä gallonoina: "))
-    if gallona < 0:
-        break
-    gallona = gallona_litroiksi(gallona)
-    print(gallona)
+# a simple function that converts gallons to litres
+
+
+def gallon_to_litres(gal: int):
+    litre = gal * 3.78541178
+    return litre
+
+
+# main asks for gallons until the user gives a negative number
+gallon = 0
+while gallon >= 0:
+    gallon = gallon_to_litres(gallon)
+    print(gallon)
+    gallon = int(input("How many gallons: "))
