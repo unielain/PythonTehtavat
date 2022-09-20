64eba8852693b5d66373f2eae25793b731f78774
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 23:13:45 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex5/5.2.py
+++ ex5/5.2.py
@@ -1,13 +1,13 @@
-luku = 0
-luvut = []
-i = 0
-while True:
-    luku = input("Anna luku: ")
-    if luku == "":
-        break
+# a simple program that asks numbers from user and prints out them in a reverse order
+number = 0
+numbers = []
 
-    luku = int(luku)
-    luvut.append(luku)
-for i in range(5):
-    luvut.sort(reverse=True)
-    print(luvut[i])
+while number != "":
+    number = input("Anna luku: ")
+    if number.isdigit():
+        number = int(number)
+        numbers.append(number)
+
+for i in range(len(numbers)):
+    numbers.sort(reverse=True)
+    print(numbers[i])
