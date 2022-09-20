bad633fd15862e8aff3d1c2ef283368036cab436
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 19:28:01 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex3/3.1..py
+++ ex3/3.1..py
@@ -1,3 +1,6 @@
-pituus = int(input("Pituus: "))
-if pituus < 37:
-    print(f"Kuha on {37 - pituus} cm alamittainen. Päästä se takaisin järveen.")
+# a simple program that measures a fish and if it is too small, tells the user to set it free.
+fish_lenght = int(input("Give the lenght of the fish: "))
+if fish_lenght < 37:
+    print(f"The fish is{37 - fish_lenght} cm too small. Let it go.")
+else:
+    print("The fish is large enough to keep.")
