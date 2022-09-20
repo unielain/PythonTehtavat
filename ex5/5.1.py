3048e69f6bedf0435842b60dc826c4eb5f032c1e
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 22:42:13 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex5/5.1.py
+++ ex5/5.1.py
@@ -1,9 +1,12 @@
 import random
-kuutioita = int(input("Montako arpakuutiota: "))
+# a program that asks the amount of dice and gives the sum of rolls
+amount_of_dice = int(input("Amount of dice: "))
 i = 0
-summa = 0
-for i in range(kuutioita):
-    luku = random.randint(1, 6)
-    summa += luku
+sum_of_rolls = 0
+
+for i in range(amount_of_dice):
+    toss = random.randint(1, 6)
+    sum_of_rolls += toss
     i += 1
-print(f"summa: {summa}")
+
+print(f"sum: {sum_of_rolls}")
