b3ffaead1a303db3f8a5d0c68c20aef3cc9f25b3
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 19:25:04 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex2/2.6.py
+++ ex2/2.6.py
@@ -1,3 +1,4 @@
 import random
+# a simple program that makes two random codes:
 print(random.randint(0, 9), random.randint(0, 9),random.randint(0, 9))
 print(random.randint(1, 6), random.randint(1, 6),random.randint(1, 6),random.randint(1, 6))
