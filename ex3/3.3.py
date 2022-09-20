ec112cb0bf2fbc7441644c3bcc1b6df098db714c
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 21:28:20 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex3/3.3.py
+++ ex3/3.3.py
@@ -1,16 +1,33 @@
-sukup = input("Oletko mies vai nainen: ")
-hmg = int(input("Anna hemoglobiiniarvosi: "))
-if sukup == "nainen":
-    if hmg > 116 and hmg < 176:
-        print("Normaali hemoglobiini")
-    elif hmg < 117:
-        print("Alhainen hemoglobiini")
+# a simple program that tells if the user has a correct, too low, or too high hmg.
+# note that this is a programming exercise and cannot be used for treating, or diagnosing illnesses consult your doctor.
+# there are only two genders available in this program since the ex. didn't provide more usable data.
+
+genders_hmgs = {"male": {
+                "min": 133,
+                "max": 196
+                },
+                "female": {
+                "min": 116,
+                "max": 176
+                }
+                }
+# we now check if user has input a correct gender
+while True:
+    gender = input("Are you a male or a female: ")
+    gender = gender.lower()
+    if gender in genders_hmgs:
+        break
     else:
-        print("Korkea hemoglobiini")
-elif sukup == "mies":
-    if hmg > 133 and hmg < 196:
-        print("Normaali hemoglobiini")
-    elif hmg < 134:
-        print("Alhainen hemoglobiini")
-    else:
-        print("Korkea hemoglobiini")
+        print(f"data for {gender} could not be found")
+
+hmg = int(input("What is your hmg: "))
+
+# now we can check the hmg for either of gender using the same code.
+for type in genders_hmgs:
+    if type == gender:
+        if genders_hmgs[gender]["min"] < hmg < genders_hmgs[gender]["max"]:
+            print("Your hemoglobine is normal level")
+        elif hmg > genders_hmgs[gender]["max"]:
+            print("Your hemoglobine is high.")
+        else:
+            print("Your hemoglobine is low.")
