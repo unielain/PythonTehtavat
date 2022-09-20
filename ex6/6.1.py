f6d9c39b42d9b5788f6a064e0bcc24a317ffe574
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 23:30:47 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex6/6.1.py
+++ ex6/6.1.py
@@ -1,11 +1,13 @@
 import random
-def heita_noppaa():
-    luku = random.randint(1, 6)
-    return luku
+# a simple program with a function without parameters that tosses a die until the result is six
+def roll_the_die():
+    roll_result = random.randint(1, 6)
+    return roll_result
 
-silmaluku = 0
-while silmaluku != 6:
-    silmaluku = heita_noppaa()
-    print(silmaluku)
-    if silmaluku == 6:
+# the main program prints the numbers
+roll_result_main = 0
+while roll_result_main != 6:
+    roll_result_main = roll_the_die()
+    print(roll_result_main)
+    if roll_result_main == 6:
         break
