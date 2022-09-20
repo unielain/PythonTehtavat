16a191f30dc9d48f516dbaebf92caeff272b9396
unto nordling <unto.nordling@metropolia.fi>
2022-09-20 23:22:55 +0300

    translated in english, cleaned code, made some improvements, learning the coding style conventions.

--- ex5/5.3.py
+++ ex5/5.3.py
@@ -1,14 +1,19 @@
-luku = int(input("Anna luku: "))
+# a simple program that detects if a number given by the user is prime number or not
+number = int(input("Anna luku: "))
 i = 0
-luvut = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-for i in range(11):
-    if luku in luvut:
-        luvut.remove(luku)
-for i in range(len(luvut)):
-    if luku % luvut[i] == 0:
-        print(f"{luku} ei ole alkuluku")
+dividers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# we can't use len as a range, since we remove one number
+# we eliminate the number given by the user and then can check if the number can be divided by others in the list
+for i in range(7):
+    if number in dividers:
+        dividers.remove(number)
+
+for i in range(len(dividers)):
+    if number % dividers[i] == 0:
+        print(f"{number} isn't a prime number")
         break
-    elif i == len(luvut) - 1:
-        if luku % luku == 0 and luku % 1 == 0:
-            print(f"{luku} on alkuluku")
+    elif i == len(dividers) - 1:
+        if number % number == 0 and number % 1 == 0:
+            print(f"{number} is a prime number")
 
